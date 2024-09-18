1655fd603b2b74e7e0bdaf95a8803667ed6d3b4e
Antoniomary <amanso.totti@yahoo.com>
2024-09-18 08:19:53 +0000

    made required_auth slash tolerant

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,10 +18,10 @@ class Auth():
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
-        if path in excluded_paths:
+        if path in excluded_paths or path + '/' in excluded_paths:
             return False
 
-        return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
