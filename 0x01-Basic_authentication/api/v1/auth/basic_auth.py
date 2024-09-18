bf8288905e9fdb0e3be89c2810b9639f583601bf
Antoniomary <amanso.totti@yahoo.com>
2024-09-18 11:10:53 +0000

    created basic_auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+contains the BasicAuth class
+"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    manages the API authentication using Basic Authentication.
+    """
+    pass
