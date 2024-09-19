1b27d37d05bfb256ea904dfa486a530f9d4e953e
Antoniomary <amanso.totti@yahoo.com>
2024-09-19 17:13:23 +0000

    added: conditional to @app.before_request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,8 +56,13 @@ def before():
 
     if auth.require_auth(request.path, ['/api/v1/status/',
                                         '/api/v1/unauthorized/',
+                                        '/api/v1/auth_session/login/',
                                         '/api/v1/forbidden/']) is False:
         return
+
+    if not auth.authorization_header(request) and \
+       not auth.session_cookie(request):
+        abort(401)
 
     if auth.authorization_header(request) is None:
         abort(401)
