e651e4a1a311211c5f63357eec43f3263f73c7b8
Antoniomary <amanso.totti@yahoo.com>
2024-09-26 09:14:46 +0000

    change import location for NoResultFound

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,9 +3,9 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
 from user import (
