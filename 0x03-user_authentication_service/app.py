ea50c5c1f2bbb49ddb123ca0bcc421a9291e8403
Antoniomary <amanso.totti@yahoo.com>
2024-09-27 00:44:26 +0000

    added post method /users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,16 @@
 """
 a flask app
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import (
+    Flask,
+    jsonify,
+    request
+)
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -13,6 +19,26 @@ def index() -> dict:
     """return a JSON payload of the form
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods={"POST"})
+def create_user() -> dict:
+    """registers a user
+    """
+    email = request.form.get('email')
+    passwd = request.form.get('password')
+
+    if not email:
+        return jsonify({"error": "email missing"})
+    if not passwd:
+        return jsonify({"error": "password missing"})
+
+    try:
+        AUTH.register_user(email, passwd)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
