91e5618121d3ed59822ff51333a6a46c51bec65d
Antoniomary <amanso.totti@yahoo.com>
2024-09-27 02:53:30 +0000

    refactored delete method /sessions

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -74,12 +74,11 @@ def logout():
     """
     session_id = request.cookies.get("session_id")
 
-    if session_id:
-        user = AUTH.get_user_from_session_id(session_id)
-        if user:
-            AUTH.destroy_session(user.id)
-        else:
-            abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
 
     return redirect('/')
 
